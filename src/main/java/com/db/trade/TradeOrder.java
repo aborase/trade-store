d1d68e148a1f110eb4e43a51c95f3b3b1511ca07
aborase <aborase@gmail.com>
2021-03-17 15:00:33 +0530

    First version trade store

---
+++ src/main/java/com/db/trade/TradeOrder.java
@@ -0,0 +1,37 @@
+/**
+ * Class to accept trade orders
+ */
+package com.db.trade;
+
+import com.db.trade.bean.Trade;
+import com.db.trade.controller.TradeController;
+import com.db.trade.store.exception.InvalidTradeException;
+
+/**
+ * Class to accept trade orders
+ * @auther Akshay Borase
+ * 
+ */
+public class TradeOrder {
+	
+	/**
+	 * Method add trade to trade store
+	 * @param trade
+	 * @throws InvalidTradeException
+	 */
+	public void addTrade(Trade trade) throws InvalidTradeException {
+		TradeController controller = new TradeController();		
+		controller.addTrade(trade);
+	}
+
+	/**
+	 * Class to accept trade orders
+	 */
+	public static void main(String[] args) {
+		
+//		TradeController controller = new TradeController();
+//		List<Trade> trades = controller.getAllTrades();
+
+	}
+
+}
